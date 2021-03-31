78442c1d311db4b2a9c62a5d081ed0e379649e92
Omkar <omihaldankar@gmail.com>
2021-03-31 11:47:45 +0530

    First Commit

---
+++ PersonalProject/src/test/java/com/omidemo/ppmtool/PersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omidemo.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
